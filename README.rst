519aaca05c9889b09348bdf5f1e5e51190e5ea87
Ewen Wang <eqwang@colostate.edu>
2017-12-12 20:39:53 +0800

    Add files via upload

---
+++ README.rst
@@ -0,0 +1,31 @@
+📦 setup.py 
+========================
+
+Purpose
+-------
+
+Simplify the process of machine learning from data cleaning, simple feature engineering, parameter tuning, to results output.
+
+Story of the GossipCat
+----------------------
+
+The package names after a cat of my friend, LEEverpool. Actually, the GossipCat is the name of a WeChat group, where my friends gossip there.
+
+.. image:: https://raw.githubusercontent.com/Ewen2015/GossipCat/master/GossipCat.jpeg
+
+Features
+--------
+
+- Combine the feature engineering and parameter tuning.
+- Automate parameter tuning with algorithms.
+- Provide accesses of the highist efficient machine learning algorithms.
+
+
+License
+-------
+
+GossipCat is licensed under the Apache License 2.0
+
+A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
+✨🍰✨
---
+++ setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+NAME = 'gossipcat'
+DESCRIPTION = 'Simplify the process of machine learning from data cleaning, simple feature engineering, parameter tuning, to results output.'
+URL = 'https://github.com/Ewen2015/GossipCat'
+EMAIL = 'Wang.Enqun@outlook.com'
+AUTHOR = 'Ewen Wang'
+REQUIRED = [
+    'pandas', 'numpy', 'lightgbm', 'sklearn', 'simulated_annealing'
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='Apache License 2.0',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
