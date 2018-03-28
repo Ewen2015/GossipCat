991fb9df9d32849eaf5943b5c39f5ad903fa346b
Ewen2015 <eqwang@colostate.edu>
2018-03-28 14:24:34 +0800

    make hypter parameter tuning more flexible

--- gossipcat/core.py
+++ gossipcat/core.py
@@ -6,6 +6,7 @@ author:     Ewen Wang
 email:      wang.enqun@outlook.com
 license:    Apache License 2.0
 """
+import numpy as np
 import lightgbm as lgb
 from lightgbm import LGBMClassifier
 from bayes_opt import BayesianOptimization
@@ -18,23 +19,33 @@ from .GraphicML import Attribute
 
 class Tune(object):
     """ Hyper parameter tuning."""
-    def __init__(self, train, target, predictors):
+    def __init__(self, train, target, predictors, metric, scoring):
+        """
+        Args: 
+            train: A training set of your machine learning project.
+            target: The target variablet; limited to binary.
+            predictors: The predictors.
+            metric: for lightgbm, 'auc';  for bayes_opt, 'binary_logloss'
+            scoring: for simulated annealing, 'roc_acu'; for sim_anneal, 'neg_log_loss'
+        """
         super(Tune, self).__init__()
         self.train = train
         self.target = target
         self.predictors = predictors
+        self.metric = metric    
+        self.scoring = scoring  
 
-    def simAnneal(self, alpha=0.75, n_jobs=1, results=True, seed=2017):
+    def simAnneal(self, alpha=0.75, n_trans=10, results=True, seed=2017):
         """ Hyper parameter tuning with simulated annealing.
 
         Employes the simulated annealing to find the optimal hyper parameters and 
         return an optimized classifier.
 
         Args:
-            train: A training set of your machine learning project.
-            target: The target variablet; limited to binary.
-            predictors: The predictors.
+            alpha:  
+            n_trans: 
             results: Whether print the progress out; default with True.
+            seed: 2017
 
         Returns:
             A classifier generated from gbdt with simulated annealing hyper parameter tuning.
@@ -48,13 +59,31 @@ class Tune(object):
         print('simulating...')
 
         gbm = LGBMClassifier(
-            learning_rate=0.01, n_estimators=5000, objective='binary', metric='auc', 
-            max_depth=2, subsample=0.75, colsample_bytree=0.75, 
-            save_binary=True, is_unbalance=True, random_state=seed
+            learning_rate=0.01, 
+            n_estimators=5000, 
+            objective='binary', 
+            metric=self.metric, 
+            max_depth=2, 
+            subsample=0.75, 
+            colsample_bytree=0.75, 
+            save_binary=True, 
+            is_unbalance=True, 
+            random_state=seed
         )
-        sa = SimulatedAnneal(gbm, params, scoring='roc_auc', T=10.0, T_min=0.001, alpha=alpha,
-                             n_trans=5, max_iter=0.25, max_runtime=300, cv=5, 
-                             random_state=seed, verbose=True, refit=True, n_jobs=n_jobs)
+        sa = SimulatedAnneal(gbm, 
+            params, 
+            scoring=self.scoring, 
+            T=10.0, 
+            T_min=0.001, 
+            alpha=alpha,
+            n_trans=n_trans, 
+            max_iter=0.25, 
+            max_runtime=300, 
+            cv=5, 
+            andom_state=seed, 
+            verbose=True, 
+            refit=True, 
+            n_jobs=1)
         
         sa.fit(self.train[self.predictors], self.train[self.target])
 
@@ -72,9 +101,6 @@ class Tune(object):
         return an optimized classifier.
 
         Args:
-            train: A training set of your machine learning project.
-            target: The target variablet; limited to binary.
-            predictors: The predictors.
             results: Whether print the progress out; default with True.
             seed: The random state.
             
@@ -101,8 +127,11 @@ class Tune(object):
             params['cosample_bytree'] = max(min(colsample_bytree, 1), 0)   
             params['subsample'] = max(min(subsample, 1), 0)
 
-            cv_result = lgb.cv(params, Dtrain, metrics='auc')
-            return cv_result['auc-mean'][-1]
+            cv_result = lgb.cv(params, Dtrain, metrics=self.metric)
+            if self.metric == 'binary_logloss':
+                return -np.array(cv_result[list(cv_result.keys())[0]]).max()
+            else:
+                return np.array(cv_result[list(cv_result.keys())[0]]).max()
 
         lgbBO = BayesianOptimization(lgb_evaluate, {'max_depth': (1, 20),
                                                     'colsample_bytree': (0.1, 1),
