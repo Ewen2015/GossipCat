be1dd8ca1332d0ea7489a82ef8a566099b5cf310
Ewen2015 <eqwang@colostate.edu>
2018-05-16 17:32:20 +0800

    employed lime to explain ML model

--- gossipcat/Develop.py
+++ gossipcat/Develop.py
@@ -13,19 +13,21 @@ import pandas_profiling as pp
 import lightgbm as lgb
 import matplotlib.pyplot as plt
 import seaborn as sns
+import lime.lime_tabular
+from IPython.core import display
+from IPython.core.display import HTML
 from .Tune import Tune
 from .Report import Report
 
 class Machine(object):
 	"""docstring for Machine"""
-	def __init__(self, wd, input_file, target, drop_list):
+	def __init__(self, wd, input_file, target, features):
 		super(Machine, self).__init__()
 		self.wd = wd
 		self.data = pd.read_csv(input_file)
 		self.train, self.test = train_test_split(self.data, test_size=0.2, random_state=0)
 		self.target = target
-		self.drop_list = drop_list
-		self.features = [i for i in self.data.columns if i not in self.drop_list]
+		self.features = features
 
 	def Summary(self):
 		print('='*20)
@@ -33,9 +35,13 @@ class Machine(object):
 		pp.ProfileReport(self.data)
 		return None
 
-	def Model(self):
+	def Model(self, wd, model_file):
 		print('='*20)
 		self.clf = Tune(train=self.train, target=self.target, predictors=self.features, metric='auc', scoring='roc_acu').simpleTrain()
+
+		print('='*20)
+		print('model saving...')
+		self.clf.save_model(wd+model_file)
 
 		print('='*20)
 		print('plotting...')
@@ -45,7 +51,7 @@ class Machine(object):
 		print('reporting...')
 		self.rpt = Report(self.clf, self.train, self.test, self.target, self.features, is_sklearn=False)
 		self.rpt.ALL()
-		return None
+		return self.clf
 
 	def Result(self, threshold=0.5):
 		print('='*20)
@@ -56,11 +62,25 @@ class Machine(object):
 		self.reuslts['probability'] = self.clf.predict(self.test[self.features])
 
 		self.results = self.results.sort_values('probability', accending=False)
-
+		display(HTML(self.results.head().to_html()))
 		print('='*20)
 		print('plotting...')
 		plt.figure(figsize=(16, 12))
 		sns.violinplot(x='truth', y='probability', hue='prediction', data=self.reuslts,
 			inner='quartile', split=True, palette='Set2')
 		return self.reuslts
+
+	def IniExplainer(self):
+		self.explainer = lime.lime_tabular.LimeTabularExplainer(np.array(self.train[self.features]),
+			feature_names=self.features, class_names=self.class_names, discretize_continous=True)
+		return None
+
+	def Explainer(self, id):
+		exp = self.explainer.explain_instance(np.array(self.test[self.features])[id], self.clf.predict, num_features=10, top_labels=1)
+		exp.show_in_notebook(show_table=True, show_all=False)
+		return None
+
+	def LoadModel(self, wd, file):
+		self.clf = lgb.Booster(model_file=wd+file)
+		return self.clf
 
