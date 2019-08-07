ad2b2e8510a6cf94e9833a08f79a2040c5679c3f
Ewen2015 <eqwang@colostate.edu>
2019-08-07 16:03:47 +0800

    revised variable names

--- gossipcat/Logging.py
+++ gossipcat/Logging.py
@@ -8,24 +8,27 @@ license:    Apache License 2.0
 """
 import logging
 
-def log_setup(log_name, log_file):
-    """Logging is one of the most underrated features. Two things (5&3) to take away from 
+def Setup(logName, logFile):
+    """To setup a logger in one step.
+
+    Logging is one of the most underrated features. Two things (5&3) to take away from 
     Logging in Python: 1) FIVE levels of importance that logs can contain(debug, info, warning, 
     error, critical); 2) THREE components to configure a logger in Python (a logger, a formatter, 
     and at least one handler).
 
     Args:
-        log_file: a target file to save loggins.
-        log_name: a logger name to display in loggings.
+        logName: a logger name to display in loggings.
+        logFile: a target file to save loggins.
+
     Return:
         logger: a well setup logger.
     """
-    logger = logging.getLogger(log_name)
+    logger = logging.getLogger(logName)
     logger.setLevel(logging.DEBUG)
     
     formatter=logging.Formatter('%(asctime)s %(name)s | %(levelname)s -> %(message)s')
     
-    file_handler = logging.FileHandler(log_file)    # creating a handler to log on the filesystem
+    file_handler = logging.FileHandler(logFile)     # creating a handler to log on the filesystem
     file_handler.setFormatter(formatter)
     file_handler.setLevel(logging.DEBUG)
 
@@ -36,13 +39,12 @@ def log_setup(log_name, log_file):
     logger.addHandler(stream_handler)               # adding handlers to our logger
     logger.addHandler(file_handler)
 
-    logger.info('happy logging')                    # test
     return logger
 
 def main():
     try:  
-        logger = log_setup(log_name='app', log_file='app.log')
-        logger.info('test')
+        logger = Setup(logName='test', logFile='test.log')
+        logger.info('this is a test')
         1/0
     except Exception as err:
         logger.error(err)
