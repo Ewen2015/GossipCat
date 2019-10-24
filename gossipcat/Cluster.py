e3d1dca38d2ef11a26478a9f043d94e81a0514ca
Ewen2015 <eqwang@colostate.edu>
2019-10-24 14:48:40 +0800

    add TimeSeries module

--- gossipcat/Cluster.py
+++ gossipcat/Cluster.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 """
-author: 	Ewen Wang
+author:     Ewen Wang
 email:      wolfgangwong2012@gmail.com
-license: 	Apache License 2.0
+license:    Apache License 2.0
 """
 import numpy as np
 import pandas as pd 
@@ -21,71 +21,71 @@ import seaborn as sns
 
 
 class Cluster(object):
-	"""docstring for Cluster"""
-	def __init__(self, data, target, features):
-		super(Cluster, self).__init__()
-		self.X = data[features]
-		self.y = data[target]
-		self.n_classes = len(self.y.unique())
+    """docstring for Cluster"""
+    def __init__(self, data, target, features):
+        super(Cluster, self).__init__()
+        self.X = data[features]
+        self.y = data[target]
+        self.n_classes = len(self.y.unique())
 
-		self.X_prep = Imputer(strategy='mean').fit_transform(self.X)
-		self.X_prep = StandardScaler().fit_transform(self.X_prep)
+        self.X_prep = Imputer(strategy='mean').fit_transform(self.X)
+        self.X_prep = StandardScaler().fit_transform(self.X_prep)
 
-	def PCAna(self):
-		pca = PCA().fit(self.X_prep)
-		features = range(pca.n_components_)
+    def PCAna(self):
+        pca = PCA().fit(self.X_prep)
+        features = range(pca.n_components_)
 
-		plt.figure(figsize=(16, 6))
-		plt.bar(features, pca.explained_variance_)
-		plt.xlabel('PCA feature')
-		plt.ylabel('variance')
-		plt.xticks(features)
-		plt.show()
-		return None 
+        plt.figure(figsize=(16, 6))
+        plt.bar(features, pca.explained_variance_)
+        plt.xlabel('PCA feature')
+        plt.ylabel('variance')
+        plt.xticks(features)
+        plt.show()
+        return None 
 
-	def KMeansAna(self):
-		self.pca_features = PCA(n_components=self.n_classes).fit_transform(self.X_prep)
-		self.cluster = KMeans(n_clusters=self.n_classes).fit(self.X_prep).labels_
-		self.comb = pd.DataFrame({
-			'pca_1': self.pca_features[:,0],
-			'pca_2': self.pca_features[:,1],
-			'cluster': self.cluster,
-			'target': self.y
-			})
+    def KMeansAna(self):
+        self.pca_features = PCA(n_components=self.n_classes).fit_transform(self.X_prep)
+        self.cluster = KMeans(n_clusters=self.n_classes).fit(self.X_prep).labels_
+        self.comb = pd.DataFrame({
+            'pca_1': self.pca_features[:,0],
+            'pca_2': self.pca_features[:,1],
+            'cluster': self.cluster,
+            'target': self.y
+            })
 
-		sns.lmplot(x='pca_1', y='pca_2', hue='cluster', col='target', data=self.comb, fit_reg=False)
-		sns.lmplot(x='pca_1', y='pca_2', hue='target', col='cluster', data=self.comb, fit_reg=False)
-		return None 
+        sns.lmplot(x='pca_1', y='pca_2', hue='cluster', col='target', data=self.comb, fit_reg=False)
+        sns.lmplot(x='pca_1', y='pca_2', hue='target', col='cluster', data=self.comb, fit_reg=False)
+        return None 
 
-	def Iris(self):
-		iris = datasets.load_iris()
-		target = 'target'
-		features = ['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid']
+    def Iris(self):
+        iris = datasets.load_iris()
+        target = 'target'
+        features = ['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid']
 
-		data = pd.DataFrame(iris.data, columns=features)
-		data[target] = iris.target
+        data = pd.DataFrame(iris.data, columns=features)
+        data[target] = iris.target
 
-		cluster = Cluster(data, target, features)
-		cluster.PCAna()
-		cluster.KMeansAna()
-		return None 
+        cluster = Cluster(data, target, features)
+        cluster.PCAna()
+        cluster.KMeansAna()
+        return None 
 
 def elbow(Xdata, clusters_range=range(1, 10)):
-	clusters_range = clusters_range
-	meanddist = []
+    clusters_range = clusters_range
+    meanddist = []
 
-	for k in clusters_range:
-		model = KMeans(n_clusters=k)
-		model.fit(Xdata)
-		meanddist.append(sum(np.min(cdist(Xdata, model.cluster_centers_, 'euclidean'), axis=1))/Xdata.shape[0])
+    for k in clusters_range:
+        model = KMeans(n_clusters=k)
+        model.fit(Xdata)
+        meanddist.append(sum(np.min(cdist(Xdata, model.cluster_centers_, 'euclidean'), axis=1))/Xdata.shape[0])
 
-	plt.figure(figsize=(8, 7))
-	plt.plot(clusters_range, meanddist)
-	plt.xlabel('Number of clusters')
-	plt.ylabel('Average distance')
-	plt.title('Selecting k with the Elbow Method')
-	plt.show()
-	return None 
+    plt.figure(figsize=(8, 7))
+    plt.plot(clusters_range, meanddist)
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Average distance')
+    plt.title('Selecting k with the Elbow Method')
+    plt.show()
+    return None 
 
 
 
---
+++ gossipcat/TimeSeries.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+author:     Ewen Wang
+email:      wolfgangwong2012@gmail.com
+license:    Apache License 2.0
+"""
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.arima_model import ARIMA
+
+def get_procedure():
+    print('Modelling procedure:\n \
+            1. Plot the data. Identify any unusual observations.\n \
+            2. If necessary, transform the data (using a Box-Cox transformation) to stabilize the variance.\n \
+            3. If the data are non-stationary: take first differences of the data until the data are stationary.\n \
+            4. Examine the ACF/PACF: Is an AR(p) or MA(q) model appropriate?\n \
+            5. Try your chosen model(s), and use the AICc to search for a better model.\n \
+            6. Check the residuals from your chosen model by plotting the ACF of the residuals, and doing a portmanteau test of the residuals. If they do not look like white noise, try a modified model.\n \
+            7. Once the residuals look like white noise, calculate forecasts.\n')
+
+def trans_time(df, time_col):
+    df[time_col] = pd.to_datetime(df[time_col])
+    return df
+
+def plot_acf_pacf(ts):
+    plot_acf(ts)
+    plot_pacf(ts)
+
+def diagnostic(df, col, order=(0, 0, 0), test_size=0.2):
+    ts = df[col].dropna().values
+
+    model = ARIMA(ts, order=order)
+    model_fit = model.fit(disp=0)
+    print(model_fit.summary())
+
+    # plot residual errors
+    residuals = pd.DataFrame(model_fit.resid)
+    residuals.plot()
+    residuals.plot(kind='kde')
+    print(residuals.describe())
+
+    # calculate MSE wity sliding windows
+    size = int(len(ts) * (1 - test_size))
+
+    train, test = ts[0:size], ts[size:len(ts)]
+    history = [x for x in train]
+
+    predictions = list()
+    for t in range(len(test)):
+        model = ARIMA(history, order=order)
+        model_fit = model.fit(disp=0)
+        output = model_fit.forecast()
+        yhat = output[0]
+        predictions.append(yhat)
+        obs = test[t]
+        history.append(obs)
+        print('predicted=%f, expected=%f' % (yhat, obs))
+    error = mean_squared_error(test, predictions)
+    print('Test MSE: %.3f' % error)
+    
+    # plot
+    plt.plot(test)
+    plt.plot(predictions, color='red')
+    plt.show()
