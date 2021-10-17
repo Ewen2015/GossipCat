3a354eb1ab7b1ae2531ff48ed1deaaa3198f694c
Ewen Wang <wang.enqun@outlook.com>
2021-10-17 23:46:30 +0800

    Update __version__.py

--- gossipcat/__version__.py
+++ gossipcat/__version__.py
@@ -3,7 +3,7 @@
 # 88YbdP88   8P   88"""   dP__Yb  Yb      88"Yb   dP__Yb  Yb  "88 88""
 # 88 YY 88  dP    88     dP""""Yb  YboodP 88  Yb dP""""Yb  YboodP 888888
 
-VERSION = (0, 1, 97)
+VERSION = (0, 1, 98)
 
 __version__ = '.'.join(map(str, VERSION))
 
