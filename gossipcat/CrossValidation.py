c79cda0e0cfe53f7f993c46fe44df25ea38da11e
Ewen Wang <wang.enqun@outlook.com>
2021-11-16 13:56:54 +0800

    update cv

--- gossipcat/CrossValidation.py
+++ gossipcat/CrossValidation.py
@@ -6,65 +6,26 @@ author:     Ewen Wang
 email:      wolfgangwong2012@gmail.com
 license:    Apache License 2.0
 """
+import pandas as pd
+import xgboost as xgb
+import pickle
+from sklearn.metrics import recall_score
 
-general_parameters = {
-    'eta': 0.1,
-    'gamma': 0,
-    'max_depth': 3,
-    'min_child_weight': 0.01,
-    'subsample': 0.75,
-    'colsample_bytree': 0.75, 
-    'colsample_bylevel': 0.75, 
-    'colsample_bynode': 1,
-    'lamdba': 5,
-    'alpha': 0.2,
-    'tree_method': 'hist'
-    # 'scale_pos_weight': scale_pos_weight
-}
-
-learning_task_parameters = {
-    'objective': 'binary:logistic',
-    'eval_metric': 'aucpr',
-    'tree_method': 'hist'
-}
-
-cv_parameters = {
-    'num_boost_round': 9000,
-    'nfold': 12,
-    'stratified': True,
-    'metrics': ('aucpr'),
-    'maximize': True,
-    'early_stopping_rounds': 20,
-    'verbose_eval': True,
-    'shuffle': True,
-    'seed': 0
-}
-
-params = {**general_parameters, **learning_task_parameters}
+from gossipcat.Logging import get_logger
+from gossipcat.Report import Visual
 
 def date_delta(date, delta, ago=True, format='%Y-%m-%d'):
-    """
-    arg:
-        date: anchor date, str
-        delta: delta days, int
-        ago: before the anchor date or after, boolean
-        format: return date format, default '%Y-%m-%d'
-    return:
-        target date: str
-    """
     from datetime import datetime, timedelta
     if ago:
         return (datetime.strptime(date, format) - timedelta(days=delta)).strftime(format)
     else:
         return (datetime.strptime(date, format) + timedelta(days=delta)).strftime(format)
 
-
 def time_fold(df, col_date, n_splits=12, delta=30*3, step=30*1):
-
     date_max = df[col_date].max()
     date_min = df[col_date].min()
     date_range = round((pd.to_datetime(date_max) - pd.to_datetime(date_min)).days/30, 2)
-    print('time window: {} to {}'.format(date_max, date_min))
+    print('time window: {} to {}'.format(date_min, date_max))
     print('time window coverage: {} months'.format(date_range))
 
     if date_range < n_splits:
@@ -90,51 +51,18 @@ def time_fold(df, col_date, n_splits=12, delta=30*3, step=30*1):
         folds.append((dtrain, dvalid))
     return folds
 
-
-def cv_rounds(data, target, features, folds, params, cv_parameters, path_cv_history):
-    dtrain = xgb.DMatrix(data[features], label=data[target])
-
-    cvr = xgb.cv(params, 
-               dtrain, 
-               num_boost_round=cv_parameters['num_boost_round'], 
-               nfold=len(folds), 
-               stratified=False, 
-               folds=folds, 
-               metrics=cv_parameters['metrics'], 
-               obj=None, 
-               feval=None, 
-               maximize=cv_parameters['maximize'], 
-               early_stopping_rounds=cv_parameters['early_stopping_rounds'], 
-               fpreproc=None, 
-               as_pandas=True, 
-               verbose_eval=cv_parameters['verbose_eval'], 
-               show_stdv=True, 
-               seed=cv_parameters['seed'], 
-               callbacks=None, 
-               shuffle=cv_parameters['shuffle'])
-    print('total rounds: {}'.format(cvr.shape[0]))
-    
-    best_round = cvr.shape[0] - cv_parameters['early_stopping_rounds']
-    print('best round: {}'.format(best_round))
-    
-    cvr.to_csv(path_cv_history)
-    print('cv history saved to {}'.format(path_cv_history))
-    return best_round
-
-
 def search(train, 
            target, 
            features,
            folds,
-           cv_parameters=cv_parameters, 
-           params=params, 
+           cv_parameters, 
+           params, 
            range_max_depth=range(3, 10, 1),
            range_subsample=range(50, 91, 5),
            range_colsample_bytree=range(50, 91, 5),
            log_path=None):
-
     esr = cv_parameters['early_stopping_rounds']
-    dtrain = xgb.DMatrix(data=train[features], label=train[target], silent=False, nthread=-1)
+    dtrain = xgb.DMatrix(data=train[features], label=train[target], silent=False, nthread=-1, enable_categorical=True)
 
     with open(log_path, 'w') as f:
         f.write('max_depth,subsample,colsample_bytree,best_round,train_aucpr_mean,train_aucpr_std,test_aucpr_mean,test_aucpr_std\n')
@@ -163,7 +91,6 @@ def search(train,
                             seed=cv_parameters['seed'], 
                             callbacks=None, 
                             shuffle=cv_parameters['shuffle'])
-
                 total_rounds = cvr.shape[0] + esr
                 print('total rounds: {}'.format(total_rounds))
                 print('early stopping rounds: {}'.format(esr))
@@ -179,6 +106,96 @@ def search(train,
                                                            cvr.tail(1)['test-aucpr-std']))
     print('done.')
     return None
+
+def cv_rounds(data, target, features, folds, params, cv_parameters, path_cv_history, logger):
+    dtrain = xgb.DMatrix(data[features], label=data[target], enable_categorical=True)
+
+    cvr = xgb.cv(params, 
+               dtrain, 
+               num_boost_round=cv_parameters['num_boost_round'], 
+               nfold=len(folds), 
+               stratified=False, 
+               folds=folds, 
+               metrics=cv_parameters['metrics'], 
+               obj=None, 
+               feval=None, 
+               maximize=cv_parameters['maximize'], 
+               early_stopping_rounds=cv_parameters['early_stopping_rounds'], 
+               fpreproc=None, 
+               as_pandas=True, 
+               verbose_eval=cv_parameters['verbose_eval'], 
+               show_stdv=True, 
+               seed=cv_parameters['seed'], 
+               callbacks=None, 
+               shuffle=cv_parameters['shuffle'])
+    logger.info('total rounds: {}'.format(cvr.shape[0]))
+    
+    best_round = cvr.shape[0]
+    logger.info('best round: {}'.format(best_round))
+    
+    cvr.to_csv(path_cv_history)
+    logger.info('cv history saved to {}'.format(path_cv_history))
+    return best_round
+
+def train(dtrain, params, cv_parameters, num_boost_round, path_model, logger):
+
+    booster = xgb.train(params, 
+                          dtrain, 
+                          num_boost_round, 
+                          evals=[(dtrain, 'train')], 
+                          obj=None, 
+                          feval=None, 
+                          maximize=cv_parameters['maximize'], 
+                          early_stopping_rounds=cv_parameters['early_stopping_rounds'], 
+                          evals_result=None, 
+                          verbose_eval=True, 
+                          xgb_model=None, 
+                          callbacks=None)
+
+    pickle.dump(booster, open(path_model, 'wb'))
+
+    logger.info('best score: {}'.format(booster.best_score))
+    logger.info('best iteration: {}'.format(booster.best_iteration))
+    return booster
+
+
+def predict(indcol, features, path_pred, path_model, path_result, logger):
+    
+    logger.info('load data from: {}'.format(path_pred))
+    df_pred = pd.read_csv(path_pred)
+    dpred = xgb.DMatrix(df_pred[features], enable_categorical=True)
+
+    logger.info('load model from: {}'.format(path_model))
+    booster = pickle.load(open(path_model, 'rb'))
+    
+    results = pd.DataFrame()
+    results[indcol] = df_pred[indcol]
+
+    logger.info('predicting')
+    results['prob'] = booster.predict(dpred)
+    results['pred'] = results['prob'].apply(lambda x: 1 if x >= 0.5 else 0)
+
+    results.to_csv(path_result, index=False)
+    logger.info('results saved in path: {}'.format(path_result))
+    
+    return results
+
+
+def test(path_result, path_pred, logger):
+    logger.info('load data from: {}'.format(path_result))
+    results = pd.read_csv(path_result)
+
+    logger.info('load data from: {}'.format(path_pred))
+    df_pred = pd.read_csv(path_pred)
+
+    results['target'] = df_pred['target']
+
+    recall = recall_score(results['target'], results['pred'], average='binary')
+    logger.info('test recall score: {}'.format(recall))
+
+    vis = Visual(results['target'], results['prob'])
+    vis.combo()
+    return recall
 
 
 
