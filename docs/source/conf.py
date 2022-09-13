75cc2d7f172187ebb5be09cf8d64f0a73d36886c
Ewen2015 <wang.enqun@outlook.com>
2022-09-13 08:15:57 +0000

    add docs

---
+++ docs/source/conf.py
@@ -0,0 +1,50 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'GossipCat'
+copyright = '2022, Ewen Wang'
+author = 'Ewen Wang'
+release = '0.3.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon',
+              'sphinx.ext.autosectionlabel']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Make sure the target is unique
+autosectionlabel_prefix_document = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- AutoDoc Mock Imports  ---------------------------------------------------
+# autodoc_mock_imports = ["batcat"]
+
+import os
+import sys
+sys.path.append(os.path.abspath('../..'))
+sys.path.append(os.path.abspath('..'))
+
+
+import pip
+
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+
+install('gossipcat')
+import gossipcat
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. GossipCat documentation master file, created by
+   sphinx-quickstart on Tue Sep 13 08:11:57 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to GossipCat's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
